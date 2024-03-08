23b5668cd25cf9170b7a2cc7aeebb1756a31ae21
Twaried <ilyasse.jmida@student.kdg.be>
2024-03-08 15:39:59 +0100

    SlideList en SubTheme in BL.Domain

--- AnswerCube/Domain/SlideList.cs
+++ AnswerCube/Domain/SlideList.cs
@@ -1,6 +1,19 @@
-namespace Domain;
+using Domain;
+
+namespace AnswerCube.BL.Domain;
 
 public class SlideList
 {
+    public string Title { get; set; }
+    public SubTheme SubTheme { get; set; }
+    public LinkedList<ISlide> Slides { get; set; }
+
+    public SlideList(string title, SubTheme subTheme, LinkedList<ISlide> slides)
+    {
+        Title = title;
+        SubTheme = subTheme;
+        Slides = slides;
+    }
+    
     
 }
--- AnswerCube/Domain/SubTheme.cs
+++ AnswerCube/Domain/SubTheme.cs
@@ -1,6 +1,13 @@
-namespace Domain;
+namespace AnswerCube.BL.Domain;
 
 public class SubTheme
 {
-    
+    public string Name { get; set; }
+    public string Description { get; set; }
+
+    public SubTheme(string name, string description)
+    {
+        Name = name;
+        Description = description;
+    }
 }
